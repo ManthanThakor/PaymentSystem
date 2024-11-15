09a57a7c7024f15774deb288734c90d7f1531878
ManthanThakor <thakormanthan849@gmail.com>
2024-11-15 16:21:52 +0530

    change 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PaymentSystem
+{
+    
+    public abstract class Payment
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \"The Payment System \"\n");
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
